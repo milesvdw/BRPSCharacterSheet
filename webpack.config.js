b14d3354f596e2ef5e918cd78e56015be2b91f8d
Miles Van de Wetering <miles.vandewetering@salesforce.com>
2024-10-13 14:07:43 -0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'production', // or 'development' for development mode
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '', // Important for GitHub Pages
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(), // Cleans the dist folder before each build
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+      inject: 'body',
+      filename: 'index.html',
+    }),
+  ],
+  devServer: {
+    static: './dist',
+    port: 3000,
+    open: true,
+    historyApiFallback: true, // For React Router
+  },
+};
