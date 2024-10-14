8a43f2b80561084b01d2970d6a735c8f9a1d7ef2
Miles Van de Wetering <miles.vandewetering@salesforce.com>
2024-10-14 10:51:28 -0700

    fix: lots of fun BS css to make it parchment-ey

--- src/CharacterSheet.tsx
+++ src/CharacterSheet.tsx
@@ -110,109 +110,120 @@ class CharacterSheet extends Component<{}, CharacterSheetState> {
 
     return (
       <div>
-        <h1 className={"CharacterHeader"}>
-          <span>{this.context?.value.personal.name} : {this.context?.value.personal.player}</span>
-          <span className="UploadDownload">
-            {this.state?.uploading && <input type="file" accept=".json" onChange={this.uploadJson} />}
-            <IconButton onClick={() => {this.promptUpload()}}><Upload /></IconButton>
-            <IconButton onClick={() => {this.downloadJson()}}><Download /></IconButton></span>
-        </h1>
+        <div id="parchment"></div>
+        <svg style={{height: 0}}>
+          <filter id="wavy2">
+            <feTurbulence x="0" y="0" baseFrequency="0.02" numOctaves="5" seed="1"></feTurbulence>
+            <feDisplacementMap in="SourceGraphic" scale="20" />
+          </filter>
+        </svg>
+        <div id="contain">
+          <h1 className={"CharacterHeader"}>
+            <span>{this.context?.value.personal.name} : {this.context?.value.personal.player}</span>
+            <span className="UploadDownload">
+              {this.state?.uploading && <input type="file" accept=".json" onChange={this.uploadJson} />}
+              <IconButton onClick={() => {this.promptUpload()}}><Upload /></IconButton>
+              <IconButton onClick={() => {this.downloadJson()}}><Download /></IconButton></span>
+          </h1>
 
-        <Collapsible title="Stats">
-          <div className="SkillsTable">
-            <div className="SkillBlocks">
-              <BaseStats
-                stats={this.context?.value.characteristics.base!} />
+          <Collapsible title="Stats">
+            <div className="SkillsTable">
+              <div className="SkillBlocks">
+                <BaseStats
+                  stats={this.context?.value.characteristics.base!} />
+              </div>
+              <div className="SkillBlocks">
+              </div>
             </div>
-            <div className="SkillBlocks">
+          </Collapsible>
+          <Collapsible title="Powers">
+            <div className="SkillsTable">
+              <div className="SkillBlocks">
+                <PowerBlock
+                  power={this.context?.value.currentStatus.power!}
+                  deletePower={this.deletePower}
+                  updatePower={this.updatePower}
+                  tag={"somaturgy"}
+                  powers={this.context?.value.powers!} />
+              </div>
+              <div className="SkillBlocks">
+                <PowerBlock
+                  power={this.context?.value.currentStatus.power!}
+                  deletePower={this.deletePower}
+                  updatePower={this.updatePower}
+                  tag={"pyromancy"}
+                  powers={this.context?.value.powers!} />
+              </div>
             </div>
-          </div>
-        </Collapsible>
-        <Collapsible title="Powers">
-          <div className="SkillsTable">
-            <div className="SkillBlocks">
-              <PowerBlock
-                power={this.context?.value.currentStatus.power!}
-                deletePower={this.deletePower}
-                updatePower={this.updatePower}
-                tag={"somaturgy"}
-                powers={this.context?.value.powers!} />
+          </Collapsible>
+          <Collapsible title="Skills">
+            <div className="SkillsTable">
+              <div className="SkillBlocks">
+                <SkillBlock
+                  deleteSkill={this.deleteSkill}
+                  updateSkill={this.updateSkill}
+                  tag="language"
+                  skills={this.context?.value.skills!} />
+                <SkillBlock
+                  deleteSkill={this.deleteSkill}
+                  updateSkill={this.updateSkill}
+                  tag="science"
+                  skills={this.context?.value.skills!} />
+                <SkillBlock
+                  deleteSkill={this.deleteSkill}
+                  updateSkill={this.updateSkill}
+                  tag="knowledge"
+                  skills={this.context?.value.skills!} />
+                <SkillBlock
+                  deleteSkill={this.deleteSkill}
+                  updateSkill={this.updateSkill}
+                  tag="craft"
+                  skills={this.context?.value.skills!} />
+              </div>
+              <div className="SkillBlocks">
+                <SkillBlock
+                  deleteSkill={this.deleteSkill}
+                  updateSkill={this.updateSkill}
+                  tag="combat"
+                  skills={this.context?.value.skills!} />
+                <SkillBlock
+                  deleteSkill={this.deleteSkill}
+                  updateSkill={this.updateSkill}
+                  tag="weapon"
+                  skills={this.context?.value.skills!} />
+                <SkillBlock
+                  deleteSkill={this.deleteSkill}
+                  updateSkill={this.updateSkill}
+                  tag="flaw"
+                  skills={this.context?.value.skills!} />
+                <SkillBlock
+                  deleteSkill={this.deleteSkill}
+                  updateSkill={this.updateSkill}
+                  tag="perception"
+                  skills={this.context?.value.skills!} />
+              </div>
+              <div className="SkillBlocks">
+                <SkillBlock
+                  deleteSkill={this.deleteSkill}
+                  updateSkill={this.updateSkill}
+                  tag="social"
+                  skills={this.context?.value.skills!} />
+                <SkillBlock
+                  deleteSkill={this.deleteSkill}
+                  updateSkill={this.updateSkill}
+                  skills={this.context?.value.skills!} />
+              </div>
             </div>
-            <div className="SkillBlocks">
-              <PowerBlock
-                power={this.context?.value.currentStatus.power!}
-                deletePower={this.deletePower}
-                updatePower={this.updatePower}
-                tag={"pyromancy"}
-                powers={this.context?.value.powers!} />
-            </div>
-          </div>
-        </Collapsible>
-        <Collapsible title="Skills">
-          <div className="SkillsTable">
-            <div className="SkillBlocks">
-              <SkillBlock
-                deleteSkill={this.deleteSkill}
-                updateSkill={this.updateSkill}
-                tag="language"
-                skills={this.context?.value.skills!} />
-              <SkillBlock
-                deleteSkill={this.deleteSkill}
-                updateSkill={this.updateSkill}
-                tag="science"
-                skills={this.context?.value.skills!} />
-              <SkillBlock
-                deleteSkill={this.deleteSkill}
-                updateSkill={this.updateSkill}
-                tag="knowledge"
-                skills={this.context?.value.skills!} />
-              <SkillBlock
-                deleteSkill={this.deleteSkill}
-                updateSkill={this.updateSkill}
-                tag="craft"
-                skills={this.context?.value.skills!} />
-            </div>
-            <div className="SkillBlocks">
-              <SkillBlock
-                deleteSkill={this.deleteSkill}
-                updateSkill={this.updateSkill}
-                tag="combat"
-                skills={this.context?.value.skills!} />
-              <SkillBlock
-                deleteSkill={this.deleteSkill}
-                updateSkill={this.updateSkill}
-                tag="weapon"
-                skills={this.context?.value.skills!} />
-              <SkillBlock
-                deleteSkill={this.deleteSkill}
-                updateSkill={this.updateSkill}
-                tag="flaw"
-                skills={this.context?.value.skills!} />
-              <SkillBlock
-                deleteSkill={this.deleteSkill}
-                updateSkill={this.updateSkill}
-                tag="perception"
-                skills={this.context?.value.skills!} />
-            </div>
-            <div className="SkillBlocks">
-              <SkillBlock
-                deleteSkill={this.deleteSkill}
-                updateSkill={this.updateSkill}
-                tag="social"
-                skills={this.context?.value.skills!} />
-              <SkillBlock
-                deleteSkill={this.deleteSkill}
-                updateSkill={this.updateSkill}
-                skills={this.context?.value.skills!} />
-            </div>
-          </div>
-        </Collapsible>
-        <Collapsible title="Inventory" open={false}>
-          <Inventory
-            deleteItem={this.deleteItem}
-            updateItem={this.updateItem}
-            items={this.context?.value.inventory!} />
-        </Collapsible>
+          </Collapsible>
+          <Collapsible title="Inventory" open={false}>
+            <Inventory
+              deleteItem={this.deleteItem}
+              updateItem={this.updateItem}
+              items={this.context?.value.inventory!} />
+          </Collapsible>
+
+        </div>
+        
         
       </div>
     );
